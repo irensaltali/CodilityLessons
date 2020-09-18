85919de1ca0bad23e72ec8bb4f7ea7d7332c6fdb
İren SALTALI <iren@saltali.com>
2020-09-18 13:09:42 +0300

    Lesson 3/1 is done

---
+++ CodilityLessons/Lesson3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons
+{
+    public class Lesson3
+    {
+        public int FrogJmp(int X, int Y, int D)
+        {
+            return (int)Math.Ceiling(((double)Y - X) / D);
+        }
+    }
+}
---
+++ CodilityLessonsTest/Lesson3Test.cs
@@ -0,0 +1,17 @@
+﻿using CodilityLessons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityLessonsTest
+{
+    [TestClass]
+    public class Lesson3Test
+    {
+        [TestMethod]
+        public void FrogJmpTest()
+        {
+            var lesson = new Lesson3();
+            Assert.AreEqual(3, lesson.FrogJmp(10, 85, 30));
+        }
+
+    }
+}
