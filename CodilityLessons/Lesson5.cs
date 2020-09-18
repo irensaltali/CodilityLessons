b20f134a66afb1078bb4c8497b7329fd5c9e5ee1
İren SALTALI <iren@saltali.com>
2020-09-18 23:25:05 +0300

    Lesson 5/1 is done

---
+++ CodilityLessons/Lesson5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons
+{
+    public class Lesson5
+    {
+        public int CountDiv(int A, int B, int K)
+        {
+            int start = A;
+            if(A % K!=0)
+                start = A + (K - A % K);
+            int end = B;
+            if(B % K !=0)
+                end = B - (B % K);
+            return (end-start)/K+1;
+        }
+    }
+}
---
+++ CodilityLessonsTest/Lesson5Test.cs
@@ -0,0 +1,23 @@
+﻿using CodilityLessons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityLessonsTest
+{
+    [TestClass]
+    public class Lesson5Test
+    {
+        [TestMethod]
+        public void CountDivTest()
+        {
+            var lesson = new Lesson5();
+            Assert.AreEqual(3, lesson.CountDiv(6,11,2));
+            Assert.AreEqual(20, lesson.CountDiv(11,345,17));
+            Assert.AreEqual(1, lesson.CountDiv(0,0,11));
+            Assert.AreEqual(1, lesson.CountDiv(10,10,5));
+        }
+
+
+        
+
+    }
+}
