66bdd4adfd59c768407011f2e78de92d305b5e5c
İren SALTALI <iren@saltali.com>
2020-09-19 23:12:42 +0300

    Lesson 6/1 is done

---
+++ CodilityLessons/Lesson6.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityLessons
+{
+    public class Lesson6
+    {
+        /// <summary>
+        /// It is too easy that way but passes all tests :)
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public int Distinct(int[] A)
+        {
+            return A.Distinct().Count();
+        }
+
+        /// <summary>
+        /// This one has same copmlexity but I didn't use linq;
+        /// </summary>
+        /// <param name="A"></param>
+        /// <returns></returns>
+        public int Distinct2(int[] A)
+        {
+            int[] v = new int[2000001];
+            for (int i = 0; i < A.Length; i++)
+            {
+                v[A[i] + 1000000]++;
+            }
+
+            int r = 0;
+            foreach (var vv in v)
+            {
+                if (vv > 0)
+                    r++;
+            }
+
+            return r;
+        }
+    }
+}
---
+++ CodilityLessonsTest/Lesson6Test.cs
@@ -0,0 +1,21 @@
+﻿using CodilityLessons;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodilityLessonsTest
+{
+    [TestClass]
+    public class Lesson6Test
+    {
+        [TestMethod]
+        public void DistinctTest()
+        {
+            var lesson = new Lesson6();
+            Assert.AreEqual(3, lesson.Distinct(new int[] { 2, 1, 1, 2, 3, 1 }));
+            Assert.AreEqual(3, lesson.Distinct2(new int[] { 2, 1, 1, 2, 3, 1 }));
+        }
+
+
+
+
+    }
+}
